eba344c73a72c27d5e56726b178e40809d9d65d6
Jass <145933956+jasspir@users.noreply.github.com>
2026-08-15 01:17:13 -0400

    fix import

--- src/main/java/you/jass/betterhitreg/utility/MultiVersion.java
+++ src/main/java/you/jass/betterhitreg/utility/MultiVersion.java
@@ -6,19 +6,18 @@ package you.jass.betterhitreg.utility;
 //version 1.20 - 1.21.11
 //import net.minecraft.client.gui.GuiGraphics;
 
-//version 26.1+
+//version 26+
 //import net.minecraft.client.gui.GuiGraphicsExtractor;
 
 //version 1.21.11-
 //import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 
-//version 26.1+
+//version 26+
 import com.mojang.blaze3d.systems.RenderSystem;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
 
 //version 1.21.11+
-import net.minecraft.client.gui.GuiGraphicsExtractor;
 import net.minecraft.gizmos.Gizmos;
 import net.minecraft.gizmos.GizmoStyle;
 
